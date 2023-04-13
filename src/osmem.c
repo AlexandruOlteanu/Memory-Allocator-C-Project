6fab6d76d17af6be7f2058349b88915a870c3563
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2023-04-13 21:34:43 +0300

    Finished

--- src/osmem.c
+++ src/osmem.c
@@ -92,47 +92,46 @@ void *allocate_memory_sbrk(size_t size)
 		}
 		void *data_memory = (const char *)full_data + block_meta_size;
 		return data_memory;
-	} else {
-		block_meta *answer = get_first_free_data_block(added_padding(size));
+	}
+	block_meta *answer = get_first_free_data_block(added_padding(size));
 
-		if (answer != NULL) {
-			answer->status = STATUS_ALLOC;
+	if (answer != NULL) {
+		answer->status = STATUS_ALLOC;
 
-			if (answer->size - added_padding(size) >= block_meta_size + 1) {
-				block_meta *current = (block_meta *)((const char *)answer + get_full_data_block_size(size));
+		if (answer->size - added_padding(size) >= block_meta_size + 1) {
+			block_meta *current = (block_meta *)((const char *)answer + get_full_data_block_size(size));
 
-				current->status = STATUS_FREE;
-				current->next = answer->next;
-				answer->next = current;
-				current->size = answer->size - get_full_data_block_size(size);
-				answer->size = added_padding(size);
-			}
-			void *data_memory = (char *)answer + block_meta_size;
-
-			return data_memory;
+			current->status = STATUS_FREE;
+			current->next = answer->next;
+			answer->next = current;
+			current->size = answer->size - get_full_data_block_size(size);
+			answer->size = added_padding(size);
 		}
-		block_meta *last = get_last_block();
-
-		if (last->status == STATUS_FREE) {
-			sbrk(added_padding(size - last->size));
-			last->status = STATUS_ALLOC;
-			last->size += added_padding(size - last->size);
-			void *data_memory = (char *)last + block_meta_size;
-			
-			return data_memory;
-		}
-		void *full_data = sbrk(get_full_data_block_size(size));
-		block_meta *current = full_data;
-
-		current->status = STATUS_ALLOC;
-		current->size = added_padding(size);
-		current->next = NULL;
-		last->next = current;
-		last = current;
-		void *data_memory = (char *)full_data + block_meta_size;
+		void *data_memory = (char *)answer + block_meta_size;
 
 		return data_memory;
 	}
+	block_meta *last = get_last_block();
+
+	if (last->status == STATUS_FREE) {
+		sbrk(added_padding(size - last->size));
+		last->status = STATUS_ALLOC;
+		last->size += added_padding(size - last->size);
+		void *data_memory = (char *)last + block_meta_size;
+
+		return data_memory;
+	}
+	void *full_data = sbrk(get_full_data_block_size(size));
+	block_meta *current = full_data;
+
+	current->status = STATUS_ALLOC;
+	current->size = added_padding(size);
+	current->next = NULL;
+	last->next = current;
+	last = current;
+	void *data_memory = (char *)full_data + block_meta_size;
+
+	return data_memory;
 }
 
 void *allocate_memory_mmap(size_t size)
@@ -151,6 +150,7 @@ void *allocate_memory_mmap(size_t size)
 		data.head = current;
 	} else {
 		block_meta *last = get_last_block();
+
 		last->next = current;
 	}
 	void *data_memory = (char *)full_data + block_meta_size;
@@ -161,6 +161,7 @@ void *allocate_memory_mmap(size_t size)
 void *os_malloc(size_t size)
 {
 	void *result = NULL;
+
 	if (get_full_data_block_size(size) < MMAP_THRESHOLD)
 		result = allocate_memory_sbrk(size);
 	else
@@ -189,89 +190,88 @@ void os_free(void *ptr)
 				prev->next = current->next;
 				current = prev;
 			}
-			if (current -> next != NULL && current->next->status == STATUS_FREE) {
+			if (current->next != NULL && current->next->status == STATUS_FREE) {
 				current->size += current->next->size + block_meta_size;
 				current->next = current->next->next;
 			}
-		} 
-    }
+		}
+	}
 }
 
 void *os_calloc(size_t nmemb, size_t size)
 {
 	size_t total_size = nmemb * size;
 	void *result = NULL;
-	if (get_full_data_block_size(total_size) < getpagesize()) {
+
+	if (get_full_data_block_size(total_size) < getpagesize())
 		result = allocate_memory_sbrk(total_size);
-	}
-	else {
+	else
 		result = allocate_memory_mmap(total_size);
-	}
-  	memset(result, 0, total_size);
-  	return result;
+	memset(result, 0, total_size);
+	return result;
 }
 
 void *os_realloc(void *ptr, size_t size)
 {
-    if (ptr == NULL) {
+	if (ptr == NULL) {
 		if (get_full_data_block_size(size) < MMAP_THRESHOLD)
-        	return allocate_memory_sbrk(size);
+			return allocate_memory_sbrk(size);
 		else
 			return allocate_memory_mmap(size);
-    }
+	}
 
-    if (!size) {
-        os_free(ptr);
-        return NULL;
-    }
+	if (!size) {
+		os_free(ptr);
+		return NULL;
+	}
 
-    block_meta *aux = (block_meta *)ptr - 1;
+	block_meta *aux = (block_meta *)ptr - 1;
 
-    if (added_padding(size) <= aux->size) {
+	if (added_padding(size) <= aux->size) {
 
 		if (aux->status == STATUS_MAPPED) {
 			if (added_padding(size) < MMAP_THRESHOLD) {
-                size_t new_size = added_padding(size);
-                void *new_memory = sbrk(MMAP_THRESHOLD);
-                if (new_memory == (void *)-1) {
-                    return NULL;
-                }
-                struct block_meta *new_block = (block_meta *)new_memory;
-                new_block->size = new_size;
-                new_block->status = STATUS_ALLOC;
-                new_block->next = aux->next;
+				size_t new_size = added_padding(size);
+				void *new_memory = sbrk(MMAP_THRESHOLD);
+				if (new_memory == (void *)-1)
+					return NULL;
+                block_meta *new_block = (block_meta *)new_memory;
+                
+				new_block->size = new_size;
+				new_block->status = STATUS_ALLOC;
+				new_block->next = aux->next;
 
-                if (data.head == aux) {
-                    data.head = new_block;
-                } else {
-                    block_meta *prev = data.head;
-                    while (prev->next != aux) {
-                        prev = prev->next;
-                    }
-                    prev->next = new_block;
-                }
+				if (data.head == aux) {
+					data.head = new_block;
+				} else {
+					block_meta *prev = data.head;
+					while (prev->next != aux) {
+						prev = prev->next;
+					}
+					prev->next = new_block;
+				}
 
-                memcpy((char *)new_memory + block_meta_size, ptr, new_size);
-                munmap(aux, aux->size + block_meta_size);
-                return (char *)new_memory + block_meta_size;
-            } 
-		}
-		else
+				memcpy((char *)new_memory + block_meta_size, ptr, new_size);
+				munmap(aux, aux->size + block_meta_size);
+				return (char *)new_memory + block_meta_size;
+			}
+		} else
 			if (aux->size - added_padding(size) >= block_meta_size + 1) {
 				block_meta *new_node = (char *)aux + get_full_data_block_size(size);
+				
 				new_node->size = aux->size - get_full_data_block_size(size);
 				new_node->status = STATUS_FREE;
 				aux->size = added_padding(size);
 				new_node->next = aux->next;
 				aux->next = new_node;
 			}
-        	return ptr;
-    } else if (aux->next && aux->next->status == STATUS_FREE && aux->size + block_meta_size + aux->next->size >= added_padding(size)) {
-        aux->size = aux->size + block_meta_size + aux->next->size;
-        aux->next = aux->next->next;
-        return ptr;
-    } else {
-        void *result = NULL;
+			return ptr;
+	} else if (aux->next && aux->next->status == STATUS_FREE && aux->size + block_meta_size + aux->next->size >= added_padding(size)) {
+		aux->size = aux->size + block_meta_size + aux->next->size;
+		aux->next = aux->next->next;
+		 return ptr;
+	} else {
+		void *result = NULL;
 		if (get_full_data_block_size(size) < MMAP_THRESHOLD) {
 			result = allocate_memory_sbrk(size);
 		}
@@ -279,10 +279,10 @@ void *os_realloc(void *ptr, size_t size)
 			result = allocate_memory_mmap(size);
 		}
 
-        if (result) {
-            memcpy(result, ptr, aux->size);
-            os_free(ptr);
-        }
-        return result;
-    }
+		if (result) {
+			memcpy(result, ptr, aux->size);
+			os_free(ptr);
+		}
+		return result;
+	}
 }
